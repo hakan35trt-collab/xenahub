abf9a037a35db509bbca3733d5613af2fff5121a
XENAHUB <xenahub@example.com>
2026-05-07 15:10:02 +0300

    fix: global Window interface with declare global block

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,13 @@
 declare module 'hpp';
 declare module '@vitejs/plugin-react';
 
-interface Window {
-  __deferredPrompt: any | null;
-  __pwaInstallable: boolean;
-  __pwaInstalled: boolean;
+declare global {
+  interface Window {
+    __deferredPrompt: any | null;
+    __pwaInstallable: boolean;
+    __pwaInstalled: boolean;
+  }
 }
+
+export {};
+
